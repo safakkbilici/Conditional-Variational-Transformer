b57ecc387811cb1601978796511be37c1f0eeb10
safak <safakk.bilici@gmail.com>
2021-09-17 17:41:08 +0300

    main trainer

---
+++ main.py
@@ -0,0 +1,13 @@
+from models.variational_transformer import CVAETransformer
+from models.trainer import train
+
+import torch
+import torch.nn as nn
+
+
+def main(args):
+    pass
+
+
+if __name__ == "__main__":
+    pass
