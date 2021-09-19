deb8e899d9b426359f922c30bb58b818a3b0d0c2
safak <safakk.bilici@gmail.com>
2021-09-19 17:06:20 +0300

    generate samples

---
+++ generate_samples.py
@@ -0,0 +1,102 @@
+import torch
+import torch.nn as nn
+
+from models.variational_transformer import CVAETransformer
+from models.eval import generate
+from tokenizer.basic_tokenizer import BasicTokenizer
+from tokenizer.bpe import BytePairTokenizer
+
+import argparse
+
+def main(args):
+    if args.tokenizer == "space":
+        tokenizer = BasicTokenizer()
+        tokenizer.load("./tokenizer")
+
+    elif args.tokenizer == "bpe":
+        tokenizer = BytePairTokenizer()
+        tokenizer.load("./tokenizer", "vocab")
+    else:
+        raise NotImplementedError()
+
+    with open(f'{args.model_params}', 'r') as fp:
+        model_params = json.load(fp)
+
+    if args.cuda == "true":
+        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    else:
+        device = torch.device("cpu")
+
+    model = CVAETransformer(**model_params)
+    model.load_state_dict(torch.load(f"{args.model}"))
+
+    samples = generate(
+        model = model,
+        device = device,
+        tokenizer = tokenizer,
+        latent_size = model_params["latent_size"],
+        n_classes = args.n_classes,
+        n_samples_per_class = args.n_generate,
+        generate_len = args.generate_len
+    )
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("--model",
+                        "-m",
+                        help="trained model",
+                        type = str,
+    )
+
+    parser.add_argument("--cuda",
+                        "-c",
+                        help="device",
+                        type = str,
+                        default = "true"
+    )
+
+    parser.add_argument("--tokenizer",
+                        "-t",
+                        help="tokenizer",
+                        type = str,
+                        default = "space"
+    )
+
+    parser.add_argument("--model_params",
+                        "-mp",
+                        help="model parameters as json file",
+                        type = str,
+                        default = "model_params.json"
+    )
+
+    parser.add_argument("--n_classes",
+                        "-ncl",
+                        help="number of classes for generating samples",
+                        type = int,
+                        default = 2
+    )
+
+    parser.add_argument("--n_generate",
+                        "-ngen",
+                        help="number of generated samples per class",
+                        type = int,
+                        default = 1
+    )
+
+    parser.add_argument("--generate_len",
+                        "-ngenl",
+                        help="max len for generation",
+                        type = int,
+                        default = 100
+    )
+
+    parser.add_argument("--evaluate_per_epoch",
+                        "-epe",
+                        help="evaluating at nth epoch",
+                        type = int,
+                        default = 2
+    )
+
+    args = parser.parse_args()
+    main(args)  
